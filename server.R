6fe31f71448619861244489f48be3ffeb63f85c9
phoenix2113 <mayur.sonawala13@gmail.com>
2017-08-23 15:46:49 +0530

    Add files via upload

---
+++ server.R
@@ -0,0 +1,30 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+shinyServer(function(input, output) {
+  
+  output$main_plot <- reactivePlot(width = 400, height = 300, function() {
+    
+    hist(faithful$eruptions,
+         probability = TRUE,
+         breaks = as.numeric(input$n_breaks),
+         xlab = "Duration (minutes)",
+         main = "Geyser eruption duration")
+    
+    if (input$individual_obs) {
+      rug(faithful$eruptions)
+    }
+    
+    if (input$density) {
+      dens <- density(faithful$eruptions, adjust = input$bw_adjust)
+      lines(dens, col = "blue")
+    }
+    
+  })
+})
